12b04a8764b4ef60218b43df4e43c072ae9eda97
PatrickPat7 <patrick.loether994@gmail.com>
2024-09-17 13:34:37 +0200

    first step

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import WeatherApp from './WeatherApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Wetter App</h1>
+      </header>
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
